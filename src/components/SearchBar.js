d942f388891e7b012bf3b290ec1160a0c3428e4d
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-09-19 11:32:06 +0100

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ImageList.css'
 
 class SearchBar extends React.Component {
     state = { term: '' };
@@ -10,14 +11,20 @@ class SearchBar extends React.Component {
 
     render() {
         return (
-            <div className="ui segment">
+            <div className="searchPadding">
                 <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Car Search</label>
-                        <input 
+                    <div>
+                        <h2><span>On</span>Splash</h2>
+                        <div className="mainText">
+                            <label>The internet's source of freely-usable images.</label> <br />
+                            <em className="poweredBy">Powered by Un-Splash's free API!</em>
+                        </div>
+                        <input
                             type="text" 
                             value={this.state.term} 
                             onChange={(e) => this.setState({ term: e.target.value })}
+                            placeholder="Search free high resolution images"
+                            className="searchBar"
                         />
                     </div>
                 </form>
